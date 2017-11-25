76c01906b1b282cac76b3d03c6b48ec12e601fbc
chairmanJ <347541621@qq.com>
2017-11-25 10:44:37 +0800

    练习2.2.2

---
+++ 选择排序/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 选择排序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SelectNum sn = new SelectNum();
+            int[] ArrayNum = { 5,8,6,12,15,13,9};
+            sn.GetNum(ArrayNum);
+            foreach(int n in ArrayNum)
+            {
+                Console.WriteLine(n + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+
+    public class SelectNum
+    {
+        int temp;
+        int num;
+        public void GetNum(int[] Array)
+        {
+            for(int i=0;i<Array.Length-1;i++)
+            {
+                num = i;
+                for(int j=i+1;j<Array.Length;j++)
+                {
+                    if(Array[j]<Array[num])
+                    {
+                        num = j;
+                    }
+                }
+                temp = Array[i];
+                Array[i] = Array[num];
+                Array[num] = temp;
+
+            }
+        }
+    }
+}
